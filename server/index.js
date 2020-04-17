b83dfc1ce3ea1ba6433a89d2aab420f5a345f0c0
Preston Shipman <pshipman2000@gmail.com>
2020-04-16 23:18:36 -0700

    work in progress

--- server/index.js
+++ server/index.js
@@ -1,13 +1,11 @@
 require('dotenv/config');
 const express = require('express');
-
 const db = require('./database');
 const ClientError = require('./client-error');
 const staticMiddleware = require('./static-middleware');
 const sessionMiddleware = require('./session-middleware');
 const fetch = require('node-fetch');
 const bcrypt = require('bcrypt');
-
 const app = express();
 
 app.use(staticMiddleware);
@@ -19,3 +17,19 @@ app.get('/api/health-check', (req, res, next) => {
     .then(result => res.json(result.rows[0]))
     .catch(err => next(err));
 });
+
+app.get('/api/notes', (req, res, next) => {
+  const allNotes = `select * from "notes"`
+  db.query(allNotes) {
+    .then(response => {
+      const notesResponse = response.rows;
+      if(!notesResponse) {
+        next(new ClientError(`No notes found!${req.method} ${req.originalUrl}`, 404));
+      }
+      else {
+        res.json(notesResponse);
+      }
+    })
+    .catch(err => { next(err); });
+  }
+})
